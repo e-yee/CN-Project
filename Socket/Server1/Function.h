40e1341b766628bf128c65b319bcb1f2fa7ff3f7
e-yee <viduydmm@gmail.com>
2024-07-30 15:59:29 +0700

    u2

--- Socket/Server1/Function.h
+++ Socket/Server1/Function.h
@@ -10,5 +10,6 @@ struct File {
 
 vector<File> getFileList(string file_name);
 void sendFileList(vector<File>& file_list, CSocket& connector);
-void uploadFile(string file_name, CSocket& connector);
+bool checkConnection(int error_code);
+void uploadFile(string file_name, CSocket& connector, int& connection);
 void uploadProcess(vector<File>& file_list, CSocket& connector);
